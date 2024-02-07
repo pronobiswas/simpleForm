3dde4fee34fac013944ccea441bc611b37392348
PRONOB BISWAS <biswaspronob@outlook.com>
2024-02-07 06:07:57 +0600

    update

---
+++ script.js
@@ -0,0 +1,79 @@
+// ===========HTML ELEMNT========
+// ===========HTML ELEMNT========
+let myform = document.querySelector("form");
+let nam = document.getElementById("names");
+let phone = document.getElementById("phone");
+let website = document.getElementById("website");
+let mails = document.getElementById("mails");
+let pass = document.getElementById("pass");
+// ==========gender=======
+let male = document.getElementById("male");
+let female = document.getElementById("female");
+let custom = document.getElementById("custom");
+// ==========checkbox========
+let ihtml = document.getElementById("ihtml");
+let icss3 = document.getElementById("icss3");
+let iscss = document.getElementById("iscss");
+let ijs = document.getElementById("ijs");
+let ilibrary = document.getElementById("ilibrary");
+let iselectOption = document.querySelector("#interest");
+let idate = document.querySelector("#idate");
+let clr = document.querySelector("#clr");
+let getReset = document.querySelector("#getReset");
+let iDone = document.querySelector("#iDone");
+// ===========HTML ELEMNT========
+// ===========HTML ELEMNT========
+
+// ============html element value=========
+// ============html element value=========
+
+// ============html element value=========
+// ============html element value=========
+iDone.addEventListener("click" , function(){
+
+    console.log(nam.value);
+    console.log(phone.value);
+    console.log(website.value);
+    console.log(mails.value);
+    console.log(pass.value);
+
+    // *******======radio value=======*******
+    let radioOption = document.querySelectorAll("input[type=radio]");
+    let radioActive;
+    for (let i = 0; i < radioOption.length; i++){
+        if(radioOption[i].checked){
+            radioActive = radioOption[i].value;
+        }
+    }
+    if(radioActive){
+        console.log(radioActive);
+    }else{
+        console.log("no option selected");
+    }
+    // *******======radio value=======*******
+
+    // =========*******checkbox********=========
+    let checkBoxOption = document.querySelectorAll("input[type=checkbox]");
+    let checkboxValue;
+    for (let i = 0; i < checkBoxOption.length; i++){
+        if (checkBoxOption[i].checked){
+            checkboxValue += " " + checkBoxOption[i].value;
+        }
+    }
+    if (checkboxValue){
+        console.log(checkboxValue);
+    }else{
+        console.log("nothing selected");
+    }
+
+    console.log(checkBoxOption);
+    // =========*******checkbox********=========
+    
+    // *******======selectOption Value=======*******
+    console.log(iselectOption.value);
+    // *******======selectOption Value=======*******
+
+})
+getReset.addEventListener("click", function(){
+    myform.reset()
+})
